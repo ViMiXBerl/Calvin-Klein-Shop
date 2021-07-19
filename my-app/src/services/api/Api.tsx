8b3ad5142c86b40d8abfc109c233f5ae5f4d3f7e
Victoria <berling2818@gmail.com>
2021-07-19 19:53:32 +0300

    Adding new products to main screen with redux

--- my-app/src/services/api/Api.tsx
+++ my-app/src/services/api/Api.tsx
@@ -1,5 +1,6 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Product from "components/Product/Product";
 import { Row, Col, Container } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,12 +15,6 @@ export const AllProducts = (): any => {
 	const productList = useSelector((state: RootState) => state.productList);
 	const { products, loading, error } = productList;
 
-	// const products = productList.products;
-	// console.log(productList);
-	console.log(products);
-	console.log(loading);
-	console.log(error);
-
 	useEffect((): any => {
 		dispath(fetchListProducts());
 	}, [dispath]);
@@ -32,10 +27,8 @@ export const AllProducts = (): any => {
 				<Message variant='danger'>{error}</Message>
 			) : (
 				<Col>
-					{console.log(products)}
-
 					<Row>
-						{products.map((product: any) => (
+						{products.map((product: any): any => (
 							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
 								<Product product={product} />
 							</Col>
@@ -48,30 +41,43 @@ export const AllProducts = (): any => {
 };
 
 export const NewProducts = (): any => {
-	const [products, setProducts] = useState<any[]>([]);
+	const dispath = useDispatch();
+	const productList = useSelector((state: RootState) => state.productList);
+	const { products, loading, error } = productList;
 
 	useEffect((): any => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get("/api/products");
+		dispath(fetchListProducts());
+	}, [dispath]);
 
-			setProducts(data);
-		};
+	// const [products, setProducts] = useState<any[]>([]);
 
-		fetchProducts();
-	}, []);
+	// useEffect((): any => {
+	// 	const fetchProducts = async () => {
+	// 		const { data } = await axios.get("/api/products");
 
-	const newProd = products.filter((product): any => product.new === true);
+	// 		setProducts(data);
+	// 	};
+
+	// 	fetchProducts();
+	// }, []);
+
+	const newProd = products.filter((product: any): any => product.new === true);
 
 	return (
 		<>
-			{/* {loading ? (<Loader/>): error ? (<Message>{error}</Message>)} */}
-			<Row>
-				{newProd.map((product): any => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error}</Message>
+			) : (
+				<Row>
+					{newProd.map((product: any): any => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</>
 	);
 };
